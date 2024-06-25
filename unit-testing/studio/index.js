b87e5504711c6be8ef7c0df1a2f2d713156fb173
SynDecency <syndecency@gmail.com>
2024-06-25 12:28:28 -0500

    Studio variable version - testing

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,33 @@
 
 let launchcode = {
-  
-}
+    organization : "nonprofit",
+    executiveDirector : "Jeff",
+    percentageCoolEmployees : 100,
+    programsOffered : ["Web Development", "Data Analysis", "Liftoff"],
+    launchOutput : function(num) {
+        divBy2 = num % 2 === 0;
+        divBy3 = num % 3 === 0;
+        divBy5 = num % 5 === 0;
+        if (divBy2 === false && divBy3 === false && divBy5 === false) {
+            return "Rutabagas! That doesn't work.";
+        } else if (divBy2 === true && divBy3 === true && divBy5 === true) {
+            return "LaunchCode Rocks!";
+        } else if (divBy2 === true && divBy3 === false && divBy5 === true) {
+            return "Launch Rocks (CRASH!!!)";
+        } else if (divBy2 === false && divBy3 === true && divBy5 === true) {
+            return "Code Rocks!";
+        } else if (divBy2 === true && divBy3 === true && divBy5 === false) {
+            return "LaunchCode!";
+        } else if (divBy2 == false && divBy3 === false && divBy5 === true) {
+            return "Rocks!";
+        } else if (divBy2 === false && divBy3 === true && divBy5 === false) {
+            return "Code!";
+        } else if(divBy2 === true && divBy3 === false && divBy5 === false) {
+            return "Launch!";
+        }
+    }
+};
 
 module.exports = launchcode;
+
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -2,7 +2,61 @@
 const launchcode = require('../index.js');
 
 describe("Testing launchcode", function(){
+  test("should return 'Jeff' for the launchcode.executiveDirector", function() {
+    expect(launchcode.executiveDirector).toBe("Jeff");
+  });
 
-  // Write your unit tests here!
-  
+  test("should return 100 for launchcode.percentageCoolEmployees", function() {
+    expect(launchcode.percentageCoolEmployees).toBe(100);
+  });
+
+  test("should return Web Development for launchcode.programsOffered index 0", function() {
+    expect(launchcode.programsOffered[0]).toEqual("Web Development");
+  });
+
+  test("should return Data Analysis for launchcode.programsOffered index 1", function() {
+    expect(launchcode.programsOffered[1]).toEqual("Data Analysis");
+  });
+
+  test("should return Liftoff for launchcode.programsOffered index 2", function() {
+    expect(launchcode.programsOffered[2]).toEqual("Liftoff");
+  });
+
+  test("should return 3 as the launchcode.programsOffered array length", function() {
+    expect(launchcode.programsOffered.length).toEqual(3);
+  });
+
+  test("should return 'Rutabagas! That doesn't work.' when the number is NOT divisible by 2, 3, or 5", function() {
+    expect(launchcode.launchOutput(17)).toBe("Rutabagas! That doesn't work.");
+  });
+
+  test("should return 'LaunchCode Rocks!' when a number is divisible by 2, 3, and 5.", function() {
+    expect(launchcode.launchOutput(30)).toBe("LaunchCode Rocks!");
+  });
+
+  test("should return 'Launch Rocks (CRASH!!!)' when a number is divisible by 2 and 5", function() {
+    expect(launchcode.launchOutput(10)).toBe("Launch Rocks (CRASH!!!)");
+  });
+
+  test("should return 'Code Rocks!' when a number is divisible by 3 and 5", function() {
+    expect(launchcode.launchOutput(15)).toBe("Code Rocks!");
+  });
+
+  test("should reutrn 'LaunchCode!' when a number is divisble by 2 and 3", function() {
+    expect(launchcode.launchOutput(6)).toBe("LaunchCode!");
+  });
+
+  test("should return 'Rocks! when a number is only divisble by 5", function() {
+    expect(launchcode.launchOutput(5)).toBe("Rocks!");
+  });
+
+  test("should return 'Code! when a number is only divisible by 3", function() {
+    expect(launchcode.launchOutput(3)).toBe("Code!");
+  });
+
+  test ("should return 'Launch! when a number is only divisible by 2", function() {
+    expect(launchcode.launchOutput(2)).toBe("Launch!");
+  })
+
+
 });
