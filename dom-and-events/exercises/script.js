5bdc1ed1c8e7ec0391a460cbd492ea7f25814aa2
SynDecency <syndecency@gmail.com>
2024-08-01 11:19:43 -0500

    DOM HW

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -1,10 +1,28 @@
-function init () {
-    const missionAbort = document.getElementById("abortMission");
-    const button = document.getElementById("liftoffButton");
-    const paragraph = document.getElementById("statusReport");
+function init() {
+  const missionAbort = document.getElementById("abortMission");
+  const button = document.getElementById("liftoffButton");
+  const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
-    
+  button.addEventListener("click", (event) => {
+    paragraph.innerHTML = "Houston! We have liftoff!";
+  });
+
+  missionAbort.addEventListener("mouseover", (event) => {
+    event.target.style.backgroundColor = "red";
+  });
+
+  missionAbort.addEventListener("mouseout", (event) => {
+    event.target.style.backgroundColor = "";
+  });
+
+  missionAbort.addEventListener("click", (event) => {
+    let response = window.confirm(
+      "Are you sure you want to abort this mission?"
+    );
+    if (response) {
+      paragraph.innerHTML = "Mission aborted! Space Shuttle returning home.";
+    }
+  });
 }
 
 window.addEventListener("load", init);
